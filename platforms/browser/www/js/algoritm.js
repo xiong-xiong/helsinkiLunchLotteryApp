90e39a0eac145a1acf527c20e2493c4bdf2be153
Juha Stenroos <juha.stenroos@edita.fi>
2018-02-28 00:09:09 +0200

    small fixes to default location before security edits

--- platforms/browser/www/js/algoritm.js
+++ platforms/browser/www/js/algoritm.js
@@ -1,7 +1,56 @@
+function restaurantLoop() {
 
+  //This part of the code gets restaurants from firebase
+  var firebaseSaveRef = firebase.database().ref("Restaurants");
+  var indexArray = [];
+  firebaseSaveRef.once("value")
+  .then(function(snapshot) {
+    snapshot.forEach(function(childSnapshot) {
+
+      var restaurants = childSnapshot.val();
+      var restaurantName = childSnapshot.child("name").val();
+      var restaurantType = childSnapshot.child("type").val();
+      var restaurantAddress = childSnapshot.child("address").val();
+      var restaurantLat = childSnapshot.child("lat").val();
+      var restaurantLng = childSnapshot.child("lon").val();
+      var restaurant = {
+        'name': restaurantName,
+        'lng': restaurantLng,
+        'lat': restaurantLat,
+        'address': restaurantAddress,
+        'type': restaurantType
+      }
+      console.log('Tämän pitäisi olla aiemmin kuin vikan');
+      restaurantLoc = {lat: restaurantLat, lng: restaurantLng};
+
+      processRestaurants(restaurantLoc, userLat, userLng, restaurantName, restaurant);
+
+      indexArray.push(restaurants);
+
+      //This measures what is the number of iteration going
+      var index = indexArray.length;
+      //This is total size of data snapshot
+      var size = snapshot.numChildren();
+
+      console.log('arrayn sisältö loopin sisällä: '+ theArray);
+
+      if(index  === size) {
+                //After last iteration of the loop we set map markers and print restaurant information
+
+                displayRestaurant();
+                setMapMarkers();
+
+
+         }
+
+  });
+
+});
+
+}
 
 function processRestaurants(restaurantLoc, userLat, userLng, restaurantName, restaurant) {
-
+//THIS IS THE ALGORITHM THAT DEFINES IS USER CLOSE ENOUGH FOR RESTAURANT TO BE INCLUDED IN LOTTERY
   var restaurantLat = restaurantLoc.lat;
   var restaurantLng = restaurantLoc.lng;
 
@@ -12,7 +61,7 @@ function processRestaurants(restaurantLoc, userLat, userLng, restaurantName, res
   lngDif = Math.abs(lngDif);
   console.log(restaurantName + ': etäisyys pituusasteina ' + lngDif);
 
-  if((lngDif < 0.135000) && (latDif < 0.135000)){
+  if((lngDif < 0.035000) && (latDif < 0.035000)){
     theArray.push(restaurant);
     console.log(restaurantName + ' on riittävän lähellä')
   }else {
@@ -37,6 +86,7 @@ function displayRestaurant() {
   restaurantLoc = {lat: chosenRestaurant.lat, lng: chosenRestaurant.lng};
   jQuery("#restaurant-info").html(
     "<h2>"+ chosenRestaurant.name +"</h2>" +
+    "<p><em>"+ chosenRestaurant.type +"</em></p>" +
     "<p>"+ chosenRestaurant.address +"</p>"
   );
 
--- www/js/map.js
+++ www/js/map.js
@@ -3,11 +3,11 @@
 OK- Hae firebasesta tieto ravintoiloiden LatLng
 OK- Erottele Käyttäjän sijainnista Lat ja lng
 OK- Vertaile ovatko riittävän lähellä (vaatii vähän laskemista). Voit myös katsoa toimisiko google mapsin distance työkalut.
-In Progress- Tee if lauseke ja lisää mukaan otettavat ravintolat arrayhin
-In Progress- Tee ravintoiloista array
+OK- Tee if lauseke ja lisää mukaan otettavat ravintolat arrayhin
+OK- Tee ravintoiloista array
 OK- Arvo numero 0 ja array.length välillä
-- Tee valitusta ravintolasta marker mapsiin ja perusta kartta myös siihen
-- Näytä ravintolan nimi yms kartan alapuolella
+OK- Tee valitusta ravintolasta marker mapsiin ja perusta kartta myös siihen
+OK- Näytä ravintolan nimi yms kartan alapuolella
 */
 
 //GLOBAL VARIABLES (SORRY)
@@ -19,7 +19,7 @@ var restaurantLoc;
 var chosenRestaurant;
 
 function initMap() {
-    //Google maps uses this as a callback function automaticly
+    //Google maps uses this as a callback function automaticly so it's kind of MAIN in this app
 
     //Check if geolocation is enabled
     if (navigator && navigator.geolocation) {
@@ -40,8 +40,7 @@ function initMap() {
       userLng = defaultLoc.lng;
       userLocation = new google.maps.LatLng(defaultLoc.lat, defaultLoc.lng);
       restaurantLoop();
-      setMapMarkers();
-      displayRestaurant();
+
     }
 
   function successCallback(position) {
@@ -51,66 +50,15 @@ function initMap() {
       userLocation = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
       restaurantLoop();
 
-      console.log('arrayn sisältö succes callbackin lopussa: '+ theArray);
+      console.log('arrayn sisältö success callbackin lopussa: '+ theArray);
 
     }
 
 }
 
 
-function restaurantLoop() {
-
-  //This part of the code gets restaurants from firebase
-  var firebaseSaveRef = firebase.database().ref("Restaurants");
-  var indexArray = [];
-  firebaseSaveRef.once("value")
-  .then(function(snapshot) {
-    snapshot.forEach(function(childSnapshot) {
-
-      var restaurants = childSnapshot.val();
-      var restaurantName = childSnapshot.child("name").val();
-      var restaurantType = childSnapshot.child("type").val();
-      var restaurantAddress = childSnapshot.child("address").val();
-      var restaurantLat = childSnapshot.child("lat").val();
-      var restaurantLng = childSnapshot.child("lon").val();
-      var restaurant = {
-        'name': restaurantName,
-        'lng': restaurantLng,
-        'lat': restaurantLat,
-        'address': restaurantAddress
-      }
-      console.log('Tämän pitäisi olla aiemmin kuin vikan');
-      restaurantLoc = {lat: restaurantLat, lng: restaurantLng};
-
-      processRestaurants(restaurantLoc, userLat, userLng, restaurantName, restaurant);
-
-      indexArray.push(restaurants);
-
-      //This measures what is the number of iteration going
-      var index = indexArray.length;
-      //This is total size of data snapshot
-      var size = snapshot.numChildren();
-
-      console.log('arrayn sisältö loopin sisällä: '+ theArray);
-
-      if(index  === size) {
-                //After last iteration of the loop we set map markers and print restaurant information
-
-                displayRestaurant();
-                setMapMarkers();
-
-
-         }
-
-  });
-
-});
-
-}
-
 
 function setMapMarkers() {
-
 
   //Draw a map with restaurantLoc as a center point
   var map = new google.maps.Map(document.getElementById('map'), {
